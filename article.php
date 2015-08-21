7bd76fc1e18d00773fe4d7f9e46209b08a356858
the2ndface <the2ndface@live.com>
2015-08-21 12:01:19 +0800

    TestGuest5.1

--- article.php
+++ article.php
@@ -15,6 +15,44 @@
  	//引入公共文件	
  	require dirname(__FILE__).'/includes/common.inc.php';
  	
+ 	//读出数据
+ 	if(isset($_GET['id'])){
+ 	    if(!!$_rows=_fetch_array("SELECT tg_id,tg_username,tg_type,tg_title,tg_content,tg_readcount,tg_commendcount,tg_date
+ 	                                  FROM tg_article
+ 	                                 WHERE tg_id='{$_GET['id']}'
+ 	                             ")){
+ 	        $_html = array();
+ 	        $_html['username'] = $_rows['tg_username'];
+ 	        $_html['title'] = $_rows['tg_title'];
+ 	        $_html['type'] = $_rows['tg_type'];
+ 	        $_html['content'] = $_rows['tg_content'];
+ 	        $_html['readcount'] = $_rows['tg_readcount'];
+ 	        $_html['commendcount'] = $_rows['tg_commendcount'];
+ 	        $_html['date'] = $_rows['tg_date'];
+ 	        
+ 	        //读取发帖用户信息
+ 	        if(!!$_rows=_fetch_array(" SELECT tg_id,tg_sex,tg_face,tg_email,tg_url
+ 	                                     FROM tg_user
+ 	                                    WHERE tg_username='{$_html['username']}'
+ 	                                  ")){
+                  //提取用户信息
+                  $_html['userid'] = $_rows['tg_id'];
+                  $_html['sex'] = $_rows['tg_sex'];
+                  $_html['face'] = $_rows['tg_face'];
+                  $_html['email'] = $_rows['tg_email'];
+                  $_html['url'] = $_rows['tg_url'];
+                  $_html=_html($_html);
+                  
+ 	        }else{
+ 	            //这个用户已被删除
+ 	        }
+ 	    }else{
+ 	        _alert_back('这个主题不存在！');
+ 	    }
+ 	}else{
+ 	    _alert_back('非法操作！');
+ 	}
+ 	
  	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -34,25 +72,28 @@
 	<div id="article">
 		<h2>帖子详情</h2>
 		<div id="subject">
-	      <dl>
-            <dd class="user">炎日(男)</dd>
-			<dt><img src="face/m01.gif" alt="炎日" /></dt>
-			<dd class="message"><a href="javascript:;" name="message" title="5">发消息</a></dd>
-			<dd class="friend"><a href="javascript:;" name="friend" title="5">加为好友</a></dd>
+  		<dl>
+			<dd class="user"><?php echo $_html['username']?>(<?php echo $_html['sex']?>)</dd>
+			<dt><img src="<?php echo $_html['face']?>" alt="<?php echo $_html['username']?>" /></dt>
+			<dd class="message"><a href="javascript:;" name="message" title="<?php echo $_html['userid']?>">发消息</a></dd>
+			<dd class="friend"><a href="javascript:;" name="friend" title="<?php echo $_html['userid']?>">加为好友</a></dd>
 			<dd class="guest">写留言</dd>
-			<dd class="flower"><a href="javascript:;" name="flower" title="5">给他送花</a></dd>
-			<dd class="email">邮件：<a href="mailto:bnbbs@163.com">bnbbs@163.com</a></dd>
-			<dd class="url">网址：<a href="http://www.yc60.com" target="_blank">http://www.yc60.com</a></dd>
-	      </dl>
-	      <div class="content">
-	        <div class="user">
-	           <span>1#</span>炎日 | 发表于：2009-04-24 00:26:42
-	        </div>
-	        <h3>主题：天掉下馅饼《游戏人生》变装拿大奖 <img src="images/icon8.gif" alt="icon" /></h3>
-	        <div class="detail">
-	                                  天上又掉馅饼了！《游戏人生》天降仙女，天降忍者，天降甲壳虫轿车……各种神仙异侠附体大行动！漂亮护士MM，超级忍者，魔幻厨师，功夫小子，埃及僵尸，变形金刚……想变什么就变什么，当侠客，还是当神仙？还是当一个科学异侠？你自己灵活选择啦！众多奇幻华丽的变身道具让你享受史上超震撼的无厘头刺激！同时各种神秘的惊喜大奖等你来拿喔！ 
-	        </div>
-	      </div>
+			<dd class="flower"><a href="javascript:;" name="flower" title="<?php echo $_html['userid']?>">给他送花</a></dd>
+			<dd class="email">邮件：<a href="mailto:<?php echo $_html['email']?>"><?php echo $_html['email']?></a></dd>
+			<dd class="url">网址：<a href="<?php echo $_html['url']?>" target="_blank"><?php echo $_html['url']?></a></dd>
+		</dl>
+		<div class="content">
+			<div class="user">
+				<span>1#</span><?php echo $_html['username']?> | 发表于：<?php echo $_html['date']?>
+			</div>
+			<h3>主题：<?php echo $_html['title']?> <img src="images/icon<?php echo $_html['type']?>.gif" alt="icon" /></h3>
+			<div class="detail">
+				<?php echo $_html['content']?>
+			</div>
+			<div class="read">
+				阅读量：(<?php echo $_html['readcount']?>) 评论量：(<?php echo $_html['commendcount']?>)
+			</div>
+		</div>
 		</div>
 		<p class="line"></p>
 		1111111111
