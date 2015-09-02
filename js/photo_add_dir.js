93fa24d39f432bda569cc3a4b19817b6a6b1c65e
the2ndface <the2ndface@live.com>
2015-09-02 11:05:49 +0800

    testguest6.9

--- js/photo_add_dir.js
+++ js/photo_add_dir.js
@@ -1,11 +1,34 @@
 window.onload = function(){
 	var type = document.getElementsByName('type');
 	var pass = document.getElementById('pass');
-	
+	var fm = document.getElementsByTagName('form')[0];
+
 	type[0].onclick = function() {
 		pass.style.display='none';
 	};
 	type[1].onclick = function() {
 		pass.style.display='block';
 	};
+	
+	fm.onsubmit = function(){
+		if (fm.name.value.length < 2 || fm.name.value.length > 40) {
+			alert('标题不得小于2位或者大于40位');
+			fm.name.value = ''; //清空
+			fm.name.focus(); //将焦点以至表单字段
+			return false;
+		}
+		
+		if(type[1].checked){
+			if (fm.password.value.length<6){
+				alert('密码不得小于6位！');
+				fm.password.value = ''; //清空
+				fm.password.focus(); //将焦点以至表单字段
+				return false;
+			}
+		}
+		
+		return true;
+	};
+	
+
 };
--- photo_add_dir.php
+++ photo_add_dir.php
@@ -13,6 +13,56 @@
  	define('SCRIPT','photo_add_dir' );
  //引入公共文件	
  	require dirname(__FILE__).'/includes/common.inc.php';
+    //管理员登录
+    _manage_login();
+    //添加目录
+    if($_GET['action']=='adddir'){
+        if(!!$_rows=_fetch_array("SELECT tg_uniqid FROM tg_user WHERE tg_username='{$_COOKIE['username']}'")){
+            //敏感操作，检查标识符
+            _uniqid($_rows['tg_uniqid'], $_COOKIE['uniqid']);
+            //接收数据
+            $_clean = array();
+            $_clean['name'] = $_POST['name'];
+            $_clean['type'] = $_POST['type'];
+            $_clean['password'] = sha1($_POST['password']);
+            $_clean['content'] = $_POST['content'];
+            $_clean['dir'] = time();
+            $_clean = _mysql_string($_clean);
+            
+            //创建目录
+            //先检查主目录是否存在
+            if(!is_dir('photo')){
+                mkdir('photo',0777);
+            }
+            //创建新目录
+            if(!is_dir('photo/'.$_clean['dir'])){
+                mkdir('photo/'.$_clean['dir'],0777);
+            }
+            //写入数据库
+            if(empty($_clean['type'])){
+                _query("INSERT INTO tg_dir (tg_name,tg_type,tg_content,tg_dir,tg_date)
+                                    VALUES ('{$_clean['name']}','{$_clean['type']}','{$_clean['content']}','photo/{$_clean['dir']}',NOW())
+                    ");
+            }else{
+                _query("INSERT INTO tg_dir (tg_name,tg_type,tg_content,tg_dir,tg_date,tg_password)
+                                    VALUES ('{$_clean['name']}','{$_clean['type']}','{$_clean['content']}','photo/{$_clean['dir']}',NOW(),'{$_clean['password']}')
+                ");
+            }
+            //目录添加成功
+            if (_affected_rows() == 1) {
+                _close();
+                _location('目录添加成功','photo.php');
+            } else {
+                _close();
+                _alert_back('目录添加失败！');
+            }
+            
+            
+        }else{
+            _alert_back('非法登录！');
+        }
+    }
+ 	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
